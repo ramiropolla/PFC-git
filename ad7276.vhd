4175eeac6b9e6a115d7225f63739e4e208239c2b
Ramiro Polla <ramiro.polla@gmail.com>
2013-06-05 01:05:01 -0300

    ad7276: fazer calculo da media movel

--- ad7276.vhd
+++ ad7276.vhd
@@ -8,15 +8,23 @@ entity ad7276 is
         i_clk: in  std_logic;
         c_ncs: out std_logic := '1';
         c_dat: in  std_logic;
-        o_dat: out unsigned(11 downto 0) := (others => '0')
+        o_dat: out unsigned(11 downto 0) := (others => '0');
+        -- media movel
+        o_mm : out unsigned(11 downto 0) := (others => '0')
     );
 end ad7276;
 
 architecture ad7276_arch of ad7276 is
+    constant mm_exp: integer := 5;
 begin
     process(i_clk)
         variable count: integer range 0 to 15 := 0;
         variable data : unsigned(11 downto 0) := (others => '0');
+        -- media movel
+        type buf_array is array (0 to 2**mm_exp-1) of unsigned(11 downto 0);
+        variable buf  : buf_array := (others => (others => '0'));
+        variable acc  : unsigned(11+mm_exp downto 0) := (others => '0');
+        variable idx  : integer range 0 to 2**mm_exp-1 := 0;
     begin
         if falling_edge(i_clk) then
             if count < 3 then
@@ -30,6 +38,12 @@ begin
             data  := data(10 downto 0) & c_dat;
             if count = 0 then
                 o_dat <= data;
+                -- media movel
+                acc := acc - buf(idx);
+                acc := acc + data;
+                buf(idx) := data;
+                o_mm  <= acc(11+mm_exp downto mm_exp);
+                idx := incrementa(idx, 2**mm_exp-1);
             end if;
         end if;
     end process;
